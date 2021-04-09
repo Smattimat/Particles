99c5ee90c6e88a2994f659f8f3936dadccb04d60
Bakuz <62158443+Bakuz@users.noreply.github.com>
2021-04-09 21:24:01 +0200

    Add files via upload

--- Mondo/FormControlli.Designer.cs
+++ Mondo/FormControlli.Designer.cs
@@ -25,14 +25,20 @@ namespace Mondo {
         /// </summary>
         private void InitializeComponent() {
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lbNumElementi = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tpControlli = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tpAbout = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
+            this.tpControlli.SuspendLayout();
+            this.tpAbout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -40,70 +46,120 @@ namespace Mondo {
             this.button1.BackColor = System.Drawing.Color.Gainsboro;
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(72, 53);
+            this.button1.Location = new System.Drawing.Point(3, 1050);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(363, 149);
+            this.button1.Size = new System.Drawing.Size(611, 122);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Esegui";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
+            // lbNumElementi
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(86, 310);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(193, 40);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Prova di testo";
+            this.lbNumElementi.AutoSize = true;
+            this.lbNumElementi.Location = new System.Drawing.Point(44, 105);
+            this.lbNumElementi.Name = "lbNumElementi";
+            this.lbNumElementi.Size = new System.Drawing.Size(127, 40);
+            this.lbNumElementi.TabIndex = 1;
+            this.lbNumElementi.Text = "Nessuno";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(216, 412);
+            this.comboBox1.Items.AddRange(new object[] {
+            "Nessuna azione"});
+            this.comboBox1.Location = new System.Drawing.Point(23, 931);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(366, 48);
+            this.comboBox1.Size = new System.Drawing.Size(572, 48);
             this.comboBox1.TabIndex = 2;
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(42, 608);
+            this.tabControl1.Controls.Add(this.tpControlli);
+            this.tabControl1.Controls.Add(this.tpAbout);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(566, 734);
+            this.tabControl1.Size = new System.Drawing.Size(633, 1389);
             this.tabControl1.TabIndex = 3;
             // 
-            // tabPage1
+            // tpControlli
             // 
-            this.tabPage1.Controls.Add(this.label2);
-            this.tabPage1.Location = new System.Drawing.Point(8, 54);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(550, 672);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tpControlli.Controls.Add(this.label1);
+            this.tpControlli.Controls.Add(this.label2);
+            this.tpControlli.Controls.Add(this.button1);
+            this.tpControlli.Controls.Add(this.lbNumElementi);
+            this.tpControlli.Controls.Add(this.comboBox1);
+            this.tpControlli.Location = new System.Drawing.Point(8, 54);
+            this.tpControlli.Name = "tpControlli";
+            this.tpControlli.Padding = new System.Windows.Forms.Padding(3);
+            this.tpControlli.Size = new System.Drawing.Size(617, 1327);
+            this.tpControlli.TabIndex = 0;
+            this.tpControlli.Text = "Controlli";
+            this.tpControlli.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 861);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 40);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Azioni disponibili";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(80, 171);
+            this.label2.Location = new System.Drawing.Point(23, 39);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(94, 40);
+            this.label2.Size = new System.Drawing.Size(279, 40);
             this.label2.TabIndex = 0;
-            this.label2.Text = "label2";
+            this.label2.Text = "Elementi nel mondo:";
             // 
-            // tabPage2
+            // tpAbout
             // 
-            this.tabPage2.Location = new System.Drawing.Point(8, 54);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(550, 672);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tpAbout.Controls.Add(this.label4);
+            this.tpAbout.Controls.Add(this.label3);
+            this.tpAbout.Controls.Add(this.pictureBox1);
+            this.tpAbout.Location = new System.Drawing.Point(8, 54);
+            this.tpAbout.Name = "tpAbout";
+            this.tpAbout.Padding = new System.Windows.Forms.Padding(3);
+            this.tpAbout.Size = new System.Drawing.Size(617, 1327);
+            this.tpAbout.TabIndex = 1;
+            this.tpAbout.Text = "About";
+            this.tpAbout.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 760);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(496, 320);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Programma di studio realizzato in C#.\r\n.Net 5\r\nWindowsForm\r\nraylib\r\n..... con l\'a" +
+    "ggiunta di qualche \r\nreminiscenza di fisica teorica.\r\n\r\nScritto in collaborazion" +
+    "e con:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Black", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.DimGray;
+            this.label3.Location = new System.Drawing.Point(14, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(327, 106);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Mondo";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Mondo.Properties.Resources.BlackHole;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 142);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(611, 597);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // FormControlli
             // 
@@ -111,32 +167,35 @@ namespace Mondo {
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(657, 1413);
             this.Controls.Add(this.tabControl1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Segoe UI", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormControlli";
-            this.Text = "FormControlli";
+            this.Text = "Mondo - FormControlli";
             this.Load += new System.EventHandler(this.FormControlli_Load);
             this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
+            this.tpControlli.ResumeLayout(false);
+            this.tpControlli.PerformLayout();
+            this.tpAbout.ResumeLayout(false);
+            this.tpAbout.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbNumElementi;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tpControlli;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tpAbout;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
---
+++ Mondo/WindowMondo.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Raylib_cs;
+
+namespace Mondo {
+    public class WindowMondo {
+        public Dati datiMondo { get; init; }
+        private int width, height;
+        private Random r = new Random();
+        private Vector2 pm1, pm2, pmDiff; // mouse delta
+
+        private float metriPerPixel;
+        private Vector2 posCentro;
+
+        private Stopwatch time = new Stopwatch();
+        private ulong nframe = 1;
+
+        public WindowMondo(int offT, int offR, int offB, int offL) {
+            Raylib.SetConfigFlags(ConfigFlag.FLAG_WINDOW_UNDECORATED | ConfigFlag.FLAG_MSAA_4X_HINT);
+            Raylib.InitWindow(640, 360, "Hello World");
+            Raylib.SetWindowPosition(offL, offT);
+            width = Raylib.GetMonitorWidth(0) - offR - offL - 5;
+            height = Raylib.GetMonitorHeight(0) - offB - offT - 5;
+            Raylib.SetWindowSize(width, height);
+
+            if (Raylib.IsWindowReady() == false) throw new Exception("Inizializzazione fallita!");
+        }
+        private int XtoScreen(float dist) {
+            return (int)((dist + posCentro.X) / metriPerPixel);
+        }
+        private int YtoScreen(float dist) {
+            return height - (int)((dist + posCentro.Y) / metriPerPixel);
+        }
+        public void Run() {
+            long elapsedms = 0, totalTemp;
+            time.Start();
+
+            Init();
+            //Raylib.SetTargetFPS(60);
+            while (!Raylib.WindowShouldClose() && datiMondo.programmaAttivo) {
+                totalTemp = time.ElapsedMilliseconds;
+                Update(elapsedms);
+                Render(elapsedms);
+                nframe++;
+                elapsedms = time.ElapsedMilliseconds - totalTemp;
+            }
+
+            Raylib.CloseWindow();
+            datiMondo.programmaAttivo = false;
+        }
+
+        // Predisposizione della configurazione iniziale della simulazione (finestra ed elementi).
+        public void Init() {
+            // Impostazione iniziale finestra
+            metriPerPixel = 100.0f / width;
+            posCentro.X = -45.0f + metriPerPixel * (width / 2);
+            posCentro.Y = -20.0f + metriPerPixel * (height / 2);
+
+            // Predisposizione iniziale elementi
+            Elemento p;
+            /*p = new Particella(new Vector2(0, 0), new Vector2(0.1f, 0.1f), 1, 1, System.Drawing.Color.Blue);
+            datiMondo.p.Add(p);
+            p = new Particella(new Vector2(0, 0), new Vector2(0.05f, 0.01f), 1, 1, System.Drawing.Color.Green);
+            datiMondo.p.Add(p);
+            p = new Particella(new Vector2(0, 0), new Vector2(0, 0), 0.5f, 1, System.Drawing.Color.Red);
+            datiMondo.p.Add(p);
+            */
+
+            /*for(int i= 0; i<500; i++) {
+                p = new Particella(new Vector2(r.Next(10), r.Next(10)), new Vector2(r.Next(1000) / 1000.0f, r.Next(1000) / 1000.0f), 0.5f, 1, System.Drawing.Color.Red);
+                datiMondo.p.Add(p);
+            }*/
+            /*for(int i=0; i<300; i++) {
+                p = new Particella(new Vector2(r.Next(15)*3, r.Next(15)*3), new Vector2(-0.3f,0.02f));
+                datiMondo.p.Add(p);
+            }*/
+
+            //datiMondo.p.Add(new Elemento(new Vector2(0, 10), new Vector2(5f, 0.3f), 0.5f, 1, System.Drawing.Color.Red));
+            //datiMondo.p.Add(new Elemento(new Vector2(0, 0), new Vector2(1, 0), 0.5f, 1, System.Drawing.Color.Bisque));
+            //datiMondo.p.Add(new Elemento(new Vector2(0, 0), new Vector2(0, 20), 10f, 1, System.Drawing.Color.DarkSeaGreen));
+
+            // 40kg e la velocità alla volata è di 400 m/s
+
+            datiMondo.p.Add(new Elemento(new Vector2(0, 10), new Vector2(1f, 0.3f), 0.5f, 1, System.Drawing.Color.Red));
+            datiMondo.p.Add(new Elemento(new Vector2(20, 10), new Vector2(-1f, 0.3f), 0.5f, 1, System.Drawing.Color.Green));
+        }
+
+        public void Update(long elapsedms) {
+            pm1 = Raylib.GetMousePosition();
+            pmDiff = pm1 - pm2;
+            pm2 = pm1;
+
+            if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_RIGHT_BUTTON)) {
+                pmDiff.Y = -pmDiff.Y;
+                posCentro = posCentro + (pmDiff * metriPerPixel);
+            }
+            if (Raylib.GetMouseWheelMove() > 0) {
+                metriPerPixel = metriPerPixel * 1.1f;
+                posCentro = posCentro * 1.1f;
+            }
+            if (Raylib.GetMouseWheelMove() < 0) {
+                metriPerPixel = metriPerPixel / 1.1f;
+                posCentro = posCentro / 1.1f;
+            }
+
+            float dt = elapsedms / 1000.0f;
+
+            foreach (Elemento p in datiMondo.p) {
+                p.posizione += (p.velocita * dt);
+                //p.velocita += (new Vector2(0, -9.81f)*dt); // Gravità
+            }
+
+            for (int i = 0; i < datiMondo.p.Count - 1; i++)
+                for (int j = i + 1; j < datiMondo.p.Count; j++) {
+                    if (datiMondo.p[i].collide(datiMondo.p[j]) == true) {
+                        datiMondo.p[i].colore = System.Drawing.Color.Yellow;
+                        datiMondo.p[j].colore = System.Drawing.Color.Yellow;
+                        // Cosa si fà adesso?
+                        // potrebbe essere utile il:
+                        // principio di conservazione della quantità di moto
+
+                    }
+                }
+
+
+        }
+
+        public void Render(long elapsedms) {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.WHITE);
+            Raylib.DrawRectangleLines(1, 1, width - 1, height - 1, Color.RED);
+
+            Raylib.DrawFPS(10, 10);
+            Raylib.DrawText("Tempo totale: " + time.ElapsedMilliseconds + " ms", 20, 40, 30, Color.BLACK);
+
+            if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_LEFT_BUTTON)) Raylib.DrawRectangle(20, 80, 25, 30, Color.RED);
+            else Raylib.DrawRectangleLines(20, 80, 25, 30, Color.RED);
+            if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_MIDDLE_BUTTON)) Raylib.DrawRectangle(50, 80, 12, 30, Color.RED);
+            else Raylib.DrawRectangleLines(50, 80, 12, 30, Color.RED);
+            if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_RIGHT_BUTTON)) Raylib.DrawRectangle(67, 80, 25, 30, Color.RED);
+            else Raylib.DrawRectangleLines(67, 80, 25, 30, Color.RED);
+
+            Raylib.DrawText("x: " + Raylib.GetMouseX() + " y: " + Raylib.GetMouseY() + " w: " +
+                Raylib.GetMouseWheelMove(), 110, 80, 30, Color.BLACK);
+
+
+            foreach (Elemento p in datiMondo.p) {
+                Raylib.DrawCircle(XtoScreen(p.posizione.X), YtoScreen(p.posizione.Y), p.raggio / metriPerPixel, p.colore.toRaylibColor());
+                Raylib.DrawCircleLines(XtoScreen(p.posizione.X), YtoScreen(p.posizione.Y), p.raggio / metriPerPixel, Color.BLACK);
+
+                Vector2 dir = Vector2.Normalize(p.velocita) * p.raggio;
+                Raylib.DrawLine(XtoScreen(p.posizione.X), YtoScreen(p.posizione.Y),
+                    XtoScreen(p.posizione.X + dir.X), YtoScreen(p.posizione.Y + dir.Y), Color.BLACK);
+
+            }
+
+
+            // Riferimenti
+            int fontSizeRif = (int)(0.25f / metriPerPixel);
+            for (int i = 1; i <= 100; i++) {
+                Raylib.DrawLine(XtoScreen(i), YtoScreen(0.1f), XtoScreen(i), YtoScreen(-0.1f), Color.PINK);
+                Raylib.DrawText(i.ToString(), XtoScreen(i - 0.1f), YtoScreen(-0.3f), fontSizeRif, Color.LIGHTGRAY);
+            }
+            for (int i = 1; i <= 20; i++) {
+                Raylib.DrawLine(XtoScreen(-0.1f), YtoScreen(i), XtoScreen(0.1f), YtoScreen(i), Color.PURPLE);
+                Raylib.DrawText(i.ToString(), XtoScreen(-0.5f), YtoScreen(i), fontSizeRif, Color.LIGHTGRAY);
+            }
+            Raylib.DrawLine(XtoScreen(0), YtoScreen(0), XtoScreen(100), YtoScreen(0), Color.LIGHTGRAY);
+            Raylib.DrawLine(XtoScreen(0), YtoScreen(0), XtoScreen(0), YtoScreen(20), Color.LIGHTGRAY);
+
+            // Assi
+            int fontSize = (int)(0.8f / metriPerPixel);
+            Raylib.DrawLine(XtoScreen(0), YtoScreen(0), XtoScreen(1), YtoScreen(0), Color.DARKBLUE);
+            Raylib.DrawTriangleLines(new Vector2(XtoScreen(0.9f), YtoScreen(0.1f)), new Vector2(XtoScreen(1), YtoScreen(0)), new Vector2(XtoScreen(0.9f), YtoScreen(-0.1f)), Color.DARKBLUE);
+            Raylib.DrawText("X", XtoScreen(1.3f), YtoScreen(0.38f), fontSize, Color.BLUE);
+
+            Raylib.DrawLine(XtoScreen(0), YtoScreen(0), XtoScreen(0), YtoScreen(1), Color.DARKGREEN);
+            Raylib.DrawTriangleLines(new Vector2(XtoScreen(0), YtoScreen(1)), new Vector2(XtoScreen(-0.1f), YtoScreen(0.9f)), new Vector2(XtoScreen(0.1f), YtoScreen(0.9f)), Color.DARKBLUE);
+            Raylib.DrawText("Y", XtoScreen(-0.28f), YtoScreen(2.0f), fontSize, Color.GREEN);
+
+            // Scala
+            Raylib.DrawLine(50, height - 50, 50 + width / 10, height - 50, Color.GRAY);
+            Raylib.DrawLine(50, height - 50, 50, height - 65, Color.GRAY);
+            Raylib.DrawLine(50 + width / 10, height - 50, 50 + width / 10, height - 65, Color.GRAY);
+            Raylib.DrawText(metriPerPixel * (width / 10.0f) + " m", 60, height - 85, 30, Color.GRAY);
+
+            Raylib.EndDrawing();
+        }
+
+
+
+
+
+    }
+}
